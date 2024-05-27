a424582cd60166e02e4f989112432d2c9a4f03b0
Niraj <nirajlande419@gmail.com>
2024-05-27 23:30:27 +0530

    update controller

--- src/main/java/com/niraj/product_service/controller/ProductController.java
+++ src/main/java/com/niraj/product_service/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.niraj.product_service.controller;
 
 import com.niraj.product_service.dto.ProductRequest;
 import com.niraj.product_service.dto.ProductResponse;
-import com.niraj.product_service.model.Product;
 import com.niraj.product_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
